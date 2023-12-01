50eb62726c29f75fb54c332dfe1500ee8209226e
Benjamin Dittes <bdittes@gmail.com>
2023-12-01 18:23:07 +0100

    ente

--- code/zeit.ts
+++ code/zeit.ts
@@ -5,9 +5,50 @@ export async function main() {
   asd.click = meinClick
 }
 
+let töne: { [key: string]: number } = { 'c': 264, 'd': 297, 'e': 330, 'f': 352, 'g': 396, 'a': 440, 'h': 494, 'c2': 528 }
+
 async function meinClick(p: Point) {
-  for (let i = 0; i < 10; ++i) {
-    asd.beep(200, 0.3, 1600)
-    asd.sleep(200)
+  asd.beep(100, 1, 10)
+  let ente = 'cdefggaaaagaaaagffffeeggggc'
+  let läng = '111122111141111411112211114'
+  for (let i = 0; i < ente.length; ++i) {
+    let t = ente[i]
+    let l = parseInt(läng[i])
+    asd.beep(l * 300 - 10, 1, töne[t])
+    asd.sleep(10)
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
